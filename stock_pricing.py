e454a5656e78ce8b8127872bb6cf52002c7eec09
Alyssa <alyssa19044@gmail.com>
2024-03-26 11:44:10 -0400

    Fixing stock pricing

--- stock_pricing.py
+++ stock_pricing.py
@@ -35,6 +35,28 @@ def create_df(ticker_lst):
     
     return ticker_dfs
 
+def mostRecentStockDate(tweet_date, when):
+    """
+    Returns the most recent stock date
+    
+    """
+    if when == 'day before':
+        day_before = str((tweet_date - pd.Timedelta(days=1)).date())
+        if day_before is None:
+            day_before = str((tweet_date - pd.Timedelta(days=3)).date())
+        return day_before
+
+    elif when == 'day after':
+        day_after = str((tweet_date + pd.Timedelta(days=1)).date())
+        if day_after is None:
+            day_after = str((tweet_date + pd.Timedelta(days=3)).date())
+        return day_after
+    
+    elif when == 'day of':
+        day_of = str((tweet_date).date())
+        
+        return day_of
+
 def add_price_data(stock_dict, tweet_df, ticker_lst):
     """Add stock price data to the tweet DataFrame.
 
@@ -52,32 +74,43 @@ def add_price_data(stock_dict, tweet_df, ticker_lst):
             # Check if the stock ticker is 'AAPL'
             if ticker == row['symbols']:
                 tweet_date = row['timestamp']
-                # Append the price data to the dataset
-                day_before = tweet_date - pd.Timedelta(days=1)
-                day_after = tweet_date + pd.Timedelta(days=1)
                 
                 # Extract the date string
-                date_str = str(day_before.date())
+                day_before = mostRecentStockData(tweet_date, 'day before')
+                day_of = mostRecentStockData(tweet_date, 'day of')
+                day_after = mostRecentStockData(tweet_date, 'day after')
                 
                 # Filter the DataFrame based on the date string
-                filtered_df = stock_df.loc[stock_df['Date'] == date_str]
+                filtered_df_before = stock_df.loc[stock_df['Date'] == day_before]
+                filtered_df_of = stock_df.loc[stock_df['Date'] == day_of]
+                filtered_df_after = stock_df.loc[stock_df['Date'] == day_after]
                 
-                if filtered_df.empty:
+                if filtered_df_before.empty:
                     price_day_before = "N/A"
+                elif filtered_df_of.empty:
+                    price_day_of = 'N/A'
+                elif filtered_df_after.empty:
+                    price_day_afterafter = 'N/A'
                 
                 else:
                     # Select the 'Close' column from the filtered DataFrame
-                    close_series = filtered_df['Close']
-
+                    close_series_before = filtered_df_before['Close']
+                    close_series_of = filtered_df_of['Close']
+                    close_series_after = filtered_df_after['Close']
+                    
                     # Convert the Series to a numpy array
-                    close_array = close_series.values
-
+                    close_array_before = close_series_before.values
+                    close_array_of = close_series_of.values
+                    close_array_after = close_series_after.values
+                    
                     # Access the first element of the numpy array
-                    price_day_before = close_array[0]
+                    price_day_before = close_array_before[0]
+                    price_day_of = close_array_of[0]
+                    price_day_after = close_array_after[0]
                 
                 #MOST RECENT DAY AFTER
                 tweet_df.at[index, 'Price Day Before Tweet'] = price_day_before
-                #tweet_df.at[index, 'Price Day of Tweet'] = price_day_of
-                #tweet_df.at[index, 'Price Day After Tweet'] = price_day_after
+                tweet_df.at[index, 'Price Day of Tweet'] = price_day_of
+                tweet_df.at[index, 'Price Day After Tweet'] = price_day_after
             
     return tweet_df
