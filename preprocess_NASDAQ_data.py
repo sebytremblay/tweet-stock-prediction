07910453d366f969c3a2b709964f3e4fb0281d47
Alyssa <alyssa19044@gmail.com>
2024-04-05 12:11:02 -0400

    made function

--- preprocess_NASDAQ_data.py
+++ preprocess_NASDAQ_data.py
@@ -1,36 +1,37 @@
 import pandas as pd  
 import yfinance as yf
 import stock_pricing as sp
-     
-# making data frame  
-df = pd.read_csv("stockerbot-export.csv", on_bad_lines='skip')  
-   
-df = df.iloc[:, :8]
-df['timestamp'] = pd.to_datetime(df['timestamp'], format='%a %b %d %H:%M:%S +0000 %Y', errors='coerce')
-df = df.dropna(subset=['timestamp'])
 
-# Access date components for each timestamp
-df['month'] = df['timestamp'].dt.month
-df['day'] = df['timestamp'].dt.day
-df['year'] = df['timestamp'].dt.year
+def create_df():     
+    # making data frame  
+    df = pd.read_csv("stockerbot-export.csv", on_bad_lines='skip')  
+    
+    df = df.iloc[:, :8]
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format='%a %b %d %H:%M:%S +0000 %Y', errors='coerce')
+    df = df.dropna(subset=['timestamp'])
 
-df['Price Day Before Tweet'] = pd.NA
-df['Price Day of Tweet'] = pd.NA
-df['Price Day After Tweet'] = pd.NA
+    # Access date components for each timestamp
+    df['month'] = df['timestamp'].dt.month
+    df['day'] = df['timestamp'].dt.day
+    df['year'] = df['timestamp'].dt.year
 
-df.head(5)
+    df['Price Day Before Tweet'] = pd.NA
+    df['Price Day of Tweet'] = pd.NA
+    df['Price Day After Tweet'] = pd.NA
 
-pd.set_option('display.max_rows', None)
-ticker_lst = df['symbols'].unique().tolist()
-print(len(ticker_lst))
+    df.head(5)
 
-stock = yf.Ticker('M')
-stock_df = stock.history(period="10y")
+    pd.set_option('display.max_rows', None)
+    ticker_lst = df['symbols'].unique().tolist()
+    print(len(ticker_lst))
 
-import stock_pricing as sp
+    stock = yf.Ticker('M')
+    stock_df = stock.history(period="10y")
 
-hi = sp.create_df(ticker_lst)
+    import stock_pricing as sp
 
-result_df = sp.add_price_data(hi, df, ticker_lst)
+    hi = sp.create_df(ticker_lst)
 
-result_df.head(20)
+    result_df = sp.add_price_data(hi, df, ticker_lst)
+
+    return result_df
